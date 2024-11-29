a1b101564b15a8f1fa438b224deeaded1a07a82c
ModxVoldHunter <65139923+ModxVoldHunter@users.noreply.github.com>
2024-11-29 03:38:43 +0000

    Changes to Configuration

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualBasic;
+using System.Net;
+
+namespace AutoSettings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            string gradlew = program.Gradle = "https://github.com/gradle/gradle/releases/latest";
+            string GradlePATH = "C:\\Windows\\Program Files\\installs\\Gradle\\";
+            string PATH = "C:\\Windows\\Program Files\\installs\\";
+            if (!File.Exists(PATH))
+            {
+                File.Create(PATH);
+                File.Create(GradlePATH);
+            }
+            else if (File.Exists(PATH))
+            {
+                File.Create(GradlePATH);
+            }
+            
+            if (File.Exists(PATH))
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile(gradlew, GradlePATH);
+            }
+        }
+            
+            
+
+        public string? Gradle { get; set; }
+    }
+}
